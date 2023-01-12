68b7716341bc991dce7e53ee393dff5591180bb8
et-codes <104942211+et-codes@users.noreply.github.com>
2023-01-11 18:05:18 -0700

    Add endpoint for categories.

--- API/Program.cs
+++ API/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt => {
@@ -26,6 +25,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Apply database migrations.
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
