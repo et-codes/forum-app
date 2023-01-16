5035c3e3465d3b87b6f074acb9f8fa23b82340b5
et-codes <104942211+et-codes@users.noreply.github.com>
2023-01-16 09:30:27 -0700

    Update to create posts, categories, users separately.

--- Infrastructure/Seed.cs
+++ Infrastructure/Seed.cs
@@ -7,12 +7,12 @@ namespace Infrastructure
     {
         public static async Task SeedData(DataContext context, UserManager<User> userManager)
         {
-            var users = new List<User>();
             var posts = new List<Post>();
             var replies = new List<Post>();
 
             if (!userManager.Users.Any())
             {
+                var users = new List<User>();                
                 users.Add(new User{DisplayName = "Eric", UserName = "eric", Email = "eric@email.com"});
                 users.Add(new User{DisplayName = "Monica", UserName = "monica", Email = "monica@email.com"});
                 users.Add(new User{DisplayName = "Max", UserName = "max", Email = "max@email.com"});
@@ -24,52 +24,56 @@ namespace Infrastructure
                 }
             }
 
-            if (
-                context.Posts.Any() 
-                || context.Categories.Any()
-            ) return;
-
-            var categories = new List<Category>
+            if (!context.Categories.Any())
             {
-                new Category
-                {
-                    CreatedDate = DateTime.UtcNow,
-                    Name = "General",
-                    Description = "General Stuff"
-                },
-            };
-
-            var rand = new Random();
-            for (int i = 1; i <= 15; i++)
-            {
-                int index = rand.Next(users.Count);
-                posts.Add(
-                    new Post{
-                        PostCategory = categories[0],
-                        CreatedDate = DateTime.UtcNow,
-                        Author = users[index],
-                        Title = $"Test post {i}",
-                        Text = "This is a test post.",
-                    }
-                );
-
-                replies.Add(
-                    new Post
+                var categories = new List<Category>();
+                categories.Add(new Category
                     {
-                        PostCategory = categories[0],
                         CreatedDate = DateTime.UtcNow,
-                        Author = users[index],
-                        InReplyTo = posts[i - 1],
-                        Title = $"Reply post {i}",
-                        Text = "This is a reply post.",
+                        Name = "General",
+                        Description = "General Stuff"
                     }
                 );
+
+                await context.Categories.AddRangeAsync(categories);
+                await context.SaveChangesAsync();
             }
 
-            await context.Categories.AddRangeAsync(categories);
-            await context.Posts.AddRangeAsync(posts);
-            await context.Posts.AddRangeAsync(replies);
-            await context.SaveChangesAsync();
+            if (!context.Posts.Any())
+            {
+                var users = userManager.Users.ToList();
+                var categories = context.Categories.ToList();
+                var rand = new Random();
+                for (int i = 1; i <= 15; i++)
+                {
+                    int index = rand.Next(users.Count - 1);
+                    posts.Add(
+                        new Post{
+                            PostCategory = categories[0],
+                            CreatedDate = DateTime.UtcNow,
+                            Author = users[index],
+                            Title = $"Test post {i}",
+                            Text = "This is a test post.",
+                        }
+                    );
+
+                    replies.Add(
+                        new Post
+                        {
+                            PostCategory = categories[0],
+                            CreatedDate = DateTime.UtcNow,
+                            Author = users[index],
+                            InReplyTo = posts[i - 1],
+                            Title = $"Reply post {i}",
+                            Text = "This is a reply post.",
+                        }
+                    );
+                }
+
+                await context.Posts.AddRangeAsync(posts);
+                await context.Posts.AddRangeAsync(replies);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
